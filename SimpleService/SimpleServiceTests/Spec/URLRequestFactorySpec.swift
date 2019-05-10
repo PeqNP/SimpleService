0945fc6e890cdc359fa27381f1c97024cd28d5c6
Eric Chamberlain <eric.chamberlain@hotmail.com>
2019-05-10 16:40:13 -0700

    Added remaining tests.

--- SimpleService/SimpleServiceTests/Spec/URLRequestFactorySpec.swift
+++ SimpleService/SimpleServiceTests/Spec/URLRequestFactorySpec.swift
@@ -22,6 +22,8 @@ class URLRequestFactorySpec: QuickSpec {
                 urlRequest = nil
             }
             
+            // MARK: `.data`
+            
             describe("POST; decoding strategy `.data`; PostBody is `Data`") {
                 struct FakeEndpoint: ServiceEndpoint {
                     typealias ResponseType = Data
@@ -53,6 +55,8 @@ class URLRequestFactorySpec: QuickSpec {
                     expect(urlRequest.httpBody).to(equal(httpBody))
                 }
             }
+            
+            // MARK: `.json`
             
             describe("POST; decoding strategy `.json`; PostBody is `Data`") {
                 struct FakeEndpoint: ServiceEndpoint {
@@ -154,7 +158,11 @@ class URLRequestFactorySpec: QuickSpec {
                 }
             }
 
-            describe("POST; decoding strategy `.keyValuePair`; PostBody is `[String: Any]`") {
+            // MARK: `.keyValue`
+            
+            // TODO: Add a test where there is more than one key/value pair. The issue is, the values in a dictionary are indeterminate. This causes the test to fail.
+            
+            describe("POST; decoding strategy `.keyValue`; PostBody is `[String: Any]`") {
                 struct FakeEndpoint: ServiceEndpoint {
                     typealias ResponseType = Data
                     enum Header { }
@@ -162,7 +170,6 @@ class URLRequestFactorySpec: QuickSpec {
                     enum QueryParameter { }
                     enum PostParameter {
                         case username(String)
-                        case password(String)
                     }
                     typealias PostBody = [PostParameter]
                     
@@ -180,16 +187,49 @@ class URLRequestFactorySpec: QuickSpec {
 
                 beforeEach {
                     subject = URLRequestFactory()
-                    let endpoint = FakeEndpoint(postBody: [.username("Eric"), .password("power9000")])
+                    let endpoint = FakeEndpoint(postBody: [.username("Eric")])
                     urlRequest = try? subject.make(from: endpoint, in: .prod)
                 }
                 
                 it("should have encoded the data correctly") {
-                    let httpBody = "username=Eric&password=power9000".data(using: .utf8)!
+                    let httpBody = "username=Eric".data(using: .utf8)!
                     expect(urlRequest.httpBody).to(equal(httpBody))
                 }
             }
 
+            // TODO: This could be supported in the future if we knew that the `Encodable` type was one-level deep.
+            describe("POST; decoding strategy `.keyValue`; PostBody is `Encodable`") {
+                struct FakeEndpoint: ServiceEndpoint {
+                    typealias ResponseType = Data
+                    enum Header { }
+                    enum PathParameter { }
+                    enum QueryParameter { }
+                    struct PostBody: Encodable {
+                        let username: String
+                    }
+
+                    var type: ServiceRequestType = .post
+                    var endpoints: Endpoints = [
+                        .prod: "https://www.example.com/"
+                    ]
+                    var postBody: PostBody?
+                    var httpBodyEncodingStrategy = HTTPBodyEncodingStrategy.keyValue
+                    
+                    init(postBody: PostBody) {
+                        self.postBody = postBody
+                    }
+                }
+                
+                beforeEach {
+                    subject = URLRequestFactory()
+                    let endpoint = FakeEndpoint(postBody: .init(username: "Eric"))
+                    urlRequest = try? subject.make(from: endpoint, in: .prod)
+                }
+                
+                it("should have returned nil; not supported") {
+                    expect(urlRequest.httpBody).to(beNil())
+                }
+            }
         }
         
     }
